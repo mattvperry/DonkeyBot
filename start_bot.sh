ca56fb44d1d57d4757aa31794b2accea354b05bd
Matthew Perry <muffinman616@gmail.com>
2015-07-20 16:21:02 -0400

    Add WOW_API_KEY to env variables

--- start_bot.sh
+++ start_bot.sh
@@ -7,6 +7,7 @@ docker run \
     -e HUBOT_GROUPME_TOKEN=${HUBOT_GROUPME_TOKEN} \
     -e HUBOT_MUMBLE_URL=${HUBOT_MUMBLE_URL} \
     -e HUBOT_MUMBLE_PASSWORD=${HUBOT_MUMBLE_PASSWORD} \
+    -e HUBOT_WOW_API_KEY=${HUBOT_WOW_API_KEY} \
     -d \
     -p 8080:8080 \
     donkeybot
