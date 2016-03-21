a5939327fdbc958434adf397d5cd0d701d570873
Matt Perry <maperr@microsoft.com>
2016-03-21 16:07:31 -0400

    Improve http scripts. Improve error handling

--- src/scripts/hscj.ts
+++ src/scripts/hscj.ts
@@ -5,40 +5,55 @@ import * as googl from "goo.gl";
 
 googl.setKey(process.env.HUBOT_GOOGLE_CSE_KEY);
 
-interface Post {
-    permalink: string;
-    title: string;
-    body: string;
-}
+interface RedditResult {
+    data: {
+        children: {
+            data: {
+                permalink: string,
+                title: string,
+                domain: string,
+                selftext: string,
+                url: string
+            };
+        }[]
+    }
+};
 
-async function reddit(res: Response, sub: string): Promise<Post> {
-    return new Promise<Post>((resolve, reject) => {
+async function getSubReddit(res: Response, sub: string, count: number): Promise<RedditResult> {
+    return new Promise<RedditResult>((resolve, reject) => {
         res
-            .http(`https://www.reddit.com/r/${sub}.json`)
-            .query({ count: 25 })
-            .get()((err, data, body) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    let results = JSON.parse(body);
-                    let data = res.random<{ data: any }>(results.data.children).data;
-                    resolve({
-                       permalink: `https://reddit.com${data.permalink}`,
-                       title: data.title,
-                       body: data.domain.match(/^self\./) ? data.selftext : data.url
-                    });
-                }
-            });
+        .http(`https://www.reddit.com/r/${sub}.json`)
+        .query({ count: count })
+        .get()((err, data, body) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(JSON.parse(body));
+            }
+        }); 
     });
 }
 
 async function onResponse(res: Response): Promise<void> {
     let sub = res.random(["hearthstone", "hearthstonecirclejerk"]);
-    let data = await reddit(res, sub);
-    res.send(data.title);
-    res.send(data.body);
-    let url = await googl.shorten(data.permalink);
-    res.send(`> ${url}`);
+    let posts = await getSubReddit(res, sub, 25);
+    let post = res.random(posts.data.children).data;
+    let url = await googl.shorten(`https://reddit.com${post.permalink}`);
+
+    let messages = [post.title];
+    let extraField = post.domain.startsWith("self") ? post.selftext : post.url;
+    if (extraField !== "") {
+        messages.push(extraField);
+    }
+
+    messages.push(`> ${url}`);
+    res.send(...messages);
 };
 
-export = (robot: Robot) => robot.respond(/(hscj)( me)?/i, onResponse);
+export = (robot: Robot) => robot.respond(/(hscj)( me)?/i, async (res) => {
+    try {
+        await onResponse(res);
+    } catch (e) {
+        robot.logger.error(e);
+    }
+});
