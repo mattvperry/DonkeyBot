6fad84fd9cba4d163df495da3159d934b22f86ed
perrym5 <muffinman616@gmail.com>
2016-03-23 18:50:23 -0400

    clean up typings

--- src/typings/goo.gl.d.ts
+++ src/typings/goo.gl.d.ts
@@ -1,11 +1,11 @@
-declare module "goo.gl" {
-    interface GoogleOptions {
-        uri?: string;
-        qs?: any;
-        encoding?: string;
-        json?: boolean;
-    }
+interface GoogleOptions {
+    uri?: string;
+    qs?: any;
+    encoding?: string;
+    json?: boolean;
+}
 
+declare module "goo.gl" {
     export function shorten(url: string, extraOptions?: GoogleOptions): Promise<any>;
     export function expand(url: string, extraOptions?: GoogleOptions): Promise<any>;
     export function analytics(url: string, extraOptions?: GoogleOptions): Promise<any>;
