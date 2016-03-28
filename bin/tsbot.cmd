a7dee86039bac48e3570305ddfa203a47dae116a
Matthew Perry <muffinman616@gmail.com>
2016-03-28 17:38:16 -0400

    Remove npm install from exes. Assume this is done beforehand

--- bin/tsbot.cmd
+++ bin/tsbot.cmd
@@ -1,3 +1,3 @@
 @echo off
 @SET PATH=%PATH%;"%~dp0..\node_modules\.bin"
-npm install --production && node_modules\.bin\tsbot.cmd --name "donkeybot" %* 
+node_modules\.bin\tsbot.cmd --name "donkeybot" %* 
