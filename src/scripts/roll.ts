d543ed102269e771fe0dbed8d1d7b7c4d9072909
Matthew Perry <muffinman616@gmail.com>
2016-10-14 10:32:11 -0400

    fix roll script

--- src/scripts/roll.ts
+++ src/scripts/roll.ts
@@ -13,7 +13,7 @@ import { Robot } from "tsbot";
 let roll = (robot: Robot) => {
     robot.respond(/roll( me)?( (\d+))?/i, (res) => {
         let max = Math.abs(Number(res.match[2])) || 100;
-        res.respond("rolls a " + Math.floor(Math.random() * max + 1) + "!");
+        res.reply("rolls a " + Math.floor(Math.random() * max + 1) + "!");
     });
 };
 
