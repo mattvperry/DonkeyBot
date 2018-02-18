2d8586d8e56635d732b6a0418cb6be729b0fcee3
Matt Perry <muffinman616@gmail.com>
2018-02-18 16:45:20 -0500

    Working music bot

--- src/scripts/discord.ts
+++ src/scripts/discord.ts
@@ -1,84 +1,31 @@
 // Description:
-//  Mumble channel announcements and music player
+//  Discord-specific bot functionality. Only works with discord adapter
+//
+// Commands:
+//  hubot play me <search> - queue a new song from youtube
+//  hubot volume me <vol> - adjust music volume
+//  hubot queue me - view current music queue
+//  hubot skip me - skip the current song
+//  hubot pause me - pause the music playback
+//  hubot resume me - resume the music playback
+//  hubot clear me - clear the current music queue
 //
 // Author:
 //  Matt Perry
 
 import { Robot } from 'hubot';
-import * as Discord from 'discord.js'
 
-interface TimeMap {
-    [user: string]: number | undefined;
-}
-
-class DiscordBot {
-    private readonly name = "Donkeybot";
-    private readonly client: Discord.Client;
-    private readonly timeMap: TimeMap = {};
-    private textChannels!: Map<string, Discord.TextChannel>;
-    private voiceChannels!: Map<string, Discord.VoiceChannel>;
-
-    constructor(private robot: Robot) {
-        this.client = new Discord.Client({
-            fetchAllMembers: true,
-            apiRequestMethod: 'burst',
-            ws: {
-                compress: true,
-                large_threshold: 1000
-            }
-        });
-    }
-
-    public connect = async (token: string) => {
-        this.client.on('ready', this.ready);
-        this.client.on('voiceStateUpdate', this.voiceStateUpdate);
-        await this.client.login(token);
-    }
-
-    private ready = async () => {
-        this.textChannels = this.groupChannels('text');
-        this.voiceChannels = this.groupChannels('voice');
-
-        const games = this.voiceChannels.get('Games');
-        if (games) {
-            await games.join();
-        }
-    }
-
-    private voiceStateUpdate = (oldMember: Discord.GuildMember, newMember: Discord.GuildMember) => {
-        const games = this.voiceChannels.get('Games');
-        const general = this.textChannels.get('general');
-        if (!games
-            || !general
-            || newMember.displayName === this.name
-            || newMember.voiceChannelID !== games.id
-            || newMember.voiceChannelID === oldMember.voiceChannelID) {
-            return;
-        }
-
-        const currentTime = Date.now();
-        const userTime = this.timeMap[newMember.displayName];
-        if (!userTime || (currentTime - userTime) > 60000) {
-            this.timeMap[newMember.displayName] = currentTime;
-            general.sendMessage(`${newMember.displayName} wants to play games!`);
-        }
-    }
-
-    private groupChannels = <T extends Discord.GuildChannel>(
-        type: 'dm' | 'group' | 'text' | 'voice' | 'category'
-    ): Map<string, T> => {
-        const channels = <T[]>this.client.channels.findAll('type', type);
-        return new Map(channels.map<[string, T]>(c => [c.name, c]));
-    }
-}
+import { DiscordBot, Player } from '../discord';
 
 export = async (robot: Robot) => {
-    if (!process.env.HUBOT_DISCORD_TOKEN) {
+    if ((<any>robot).adapterName !== 'discord') {
         return;
     }
 
+    const client = (<any>robot).adapter.client;
+    const bot = new DiscordBot(robot, client);
     try {
-        await (new DiscordBot(robot)).connect(process.env.HUBOT_DISCORD_TOKEN);
+        await bot.connect();
     } catch (e) {
         console.error(e);
     }
