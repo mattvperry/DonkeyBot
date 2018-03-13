e0167d928e8e81694e93566558a8853bd61538b7
Matt Perry <muffinman616@gmail.com>
2018-03-13 17:52:13 -0400

    Cleanup durations

--- bin/donkeybot.js
+++ bin/donkeybot.js
@@ -1,3 +1,7 @@
+var moment = require('moment');
+var momentDurationFormat = require("moment-duration-format");
+momentDurationFormat(moment);
+
 require('ts-node/register');
 require('coffee-script/register');
 require('../node_modules/hubot/bin/hubot.js');
