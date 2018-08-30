c48d1425268e497b97f0fc49d9d75922efeae710
Matt Perry <muffinman616@gmail.com>
2018-08-30 02:21:54 -0400

    Fix sniperino stats

--- src/scripts/sniperino.ts
+++ src/scripts/sniperino.ts
@@ -9,11 +9,7 @@
 // Author:
 //  Steve Shipsey
 
-import { Response, Robot, User } from 'hubot';
-
-interface Snipers {
-    [id: string]: Sniper;
-}
+import { Response, Robot, User, Brain } from 'hubot';
 
 class Sniper {
     public name: string;
@@ -47,31 +43,35 @@ class Sniper {
     }
 }
 
-export = (robot: Robot) => {
-    const brain = robot.brain;
+const getSnipers = (brain: Brain): Record<string, Sniper> => {
     // If the brain has no knowledge of sniperino, create it
     if (brain.get('sniperino') === null) {
         brain.set('sniperino', {});
     }
 
-    // Create a constant sniperino
-    const sniperino: Snipers = brain.get('sniperino');
+    return brain.get('sniperino');
+}
 
-    // Get a sniper with a given name. If one doesn't exist, create it.
-    const getOrCreateSniper = (user: User, create: boolean) => {
-        // Check for active sniper, if not, create one
-        if (sniperino[user.id] === undefined && create) {
-            sniperino[user.id] = new Sniper(user);
-        }
+// Get a sniper with a given name. If one doesn't exist, create it.
+const getOrCreateSniper = (brain: Brain, user: User, create: boolean) => {
+    const snipers = getSnipers(brain);
 
-        return sniperino[user.id];
-    };
+    // Check for active sniper, if not, create one
+    if (snipers[user.id] === undefined && create) {
+        snipers[user.id] = new Sniper(user);
+    }
+
+    return snipers[user.id];
+};
+
+export = (robot: Robot) => {
 
     // Respond to a stats message
     robot.respond(/sniperino stats( me)?/i, res => {
-        const stats = Object.keys(sniperino)
-            .sort((a, b) => sniperino[b].getWinRate() - sniperino[a].getWinRate())
-            .map(n => `${sniperino[n].name}: ${sniperino[n].getWinRate()}%`);
+        const snipers = getSnipers(robot.brain);
+        const stats = Object.keys(snipers)
+            .sort((a, b) => snipers[b].getWinRate() - snipers[a].getWinRate())
+            .map(n => `${snipers[n].name}: ${snipers[n].getWinRate()}%`);
 
         res.send(stats.join('\n'));
     });
@@ -79,7 +79,7 @@ export = (robot: Robot) => {
     // Respond to a sniperino message
     robot.respond(/sniperino( me)?$/i, res => {
         // Define instance of our Sniper object from getSniper method
-        const sniper = getOrCreateSniper(res.message.user, true);
+        const sniper = getOrCreateSniper(robot.brain, res.message.user, true);
 
         // Deny the play if someone is already actively sniping. Otheriwise, display their roll.
         if (sniper.currentSnipe === null) {
@@ -87,27 +87,23 @@ export = (robot: Robot) => {
             sniper.generateNewSnipe();
 
             // Send their game information
-            // tslint:disable-next-line:max-line-length
             res.send(`ヽ༼ຈل͜ຈ༽_•︻̷̿┻̿═━一 ༼ つ ಥ_ಥ ༽つ${sniper.name}, roll higher than a ${sniper.currentSnipe} or the donger gets it!`);
         } else {
             // Player is already playing, yell at them because they are an idiot
-            // tslint:disable-next-line:max-line-length
             res.send(`(ノಠ益ಠ)ノ彡┻━┻ YOU ARE ALREADY PLAYING SNIPERINO, ${sniper.name}. I oughtta sniperino YOU! ༼ຈل͜ຈ༽_•︻̷̿┻̿═━一`);
         }
     });
 
     robot.on('roll', (res: Response, roll: number, max: number) => {
         // Get our currently rolling sniper
-        const sniper = getOrCreateSniper(res.message.user, false);
+        const sniper = getOrCreateSniper(robot.brain, res.message.user, false);
 
         // If this is a valid sniperino roll and we have an active sniperino, resolve the game
         if (max === 100 && sniper.currentSnipe) {
             if (roll > sniper.currentSnipe) {
-                // tslint:disable-next-line:max-line-length
                 res.send(`(◠‿◠✿) ${sniper.name}, you roll a ${roll} and the donger lives! The donger thanks you (◠‿◠✿)`);
                 sniper.gamesWon += 1;
             } else if (roll === sniper.currentSnipe) {
-                // tslint:disable-next-line:max-line-length
                 res.send(`ヽ༼ຈل͜ຈ༽/ ${sniper.name}, you roll a ${roll} and tie! The donger is merely wounded. He will recover! ヽ༼ຈل͜ຈ༽/`);
             } else {
                 res.send(`༼ つ x_x ༽つThe donger is dead. ${sniper.name}, you did this! You MONSTER! ༼ つ x_x ༽ つ`);
