a52f12ecf7b9680cfa26fff2d3f63427f2dac054
Matt Perry <muffinman616@gmail.com>
2018-08-30 21:40:51 -0400

    Sniperino 2.0

--- src/scripts/sniperino.ts
+++ src/scripts/sniperino.ts
@@ -8,122 +8,129 @@
 //
 // Author:
 //  Steve Shipsey
+//  Matt Perry
 
-import { Brain, Response, Robot, User } from 'hubot';
+import { Brain, Response, Robot } from 'hubot';
 
-interface Game {
-    readonly userId: string;
+enum GameEvent {
+    New = 'new',
+    Dupe = 'dupe',
+    Win = 'win',
+    Draw = 'draw',
+    Lose = 'lose',
 }
 
 interface PlayerStats {
-    readonly userId: string;
     readonly gamesPlayed: number;
     readonly gamesWon: number;
+    readonly userId: string;
 }
+
+interface SniperState {
+    readonly games: Partial<Record<string, number>>;
+    readonly stats: Partial<Record<string, PlayerStats>>;
+}
+
+interface GameContext {
+    readonly name: string;
+    readonly snipe?: number;
+    readonly roll?: number;
+}
+
+const loadState = (brain: Brain): SniperState => {
+    const state = brain.get<SniperState>('sniperino');
+    if (state) {
+        return state;
+    }
+
+    const newState = { games: {}, stats: {} };
+    brain.set('sniperino', newState);
+    return newState;
+};
+
+const playGame = (roll: number, snipe: number) => {
+    if (roll > snipe) {
+        return GameEvent.Win;
+    }
+
+    if (roll === snipe) {
+        return GameEvent.Draw;
+    }
+
+    return GameEvent.Lose;
+};
 
 const calculateWinRate = ({ gamesPlayed, gamesWon }: PlayerStats) =>
     gamesPlayed > 0 ? Math.round(gamesWon / gamesPlayed * 10000) / 100 : 0;
 
-class Sniper {
-    public name: string;
-    public gamesPlayed: number;
-    public gamesWon: number;
-    public currentSnipe: number | null;
-
-    constructor(user: User) {
-        this.name = user.name.split(' ').length > 1 ? user.name.split(' ')[1] : user.name;
-
-        if (user.name === 'Andrew Janucik') {
-            this.name = 'Guiseppe';
-        }
-
-        if (user.name === 'Joe Kim') {
-            this.name = 'Jow';
-        }
-
-        this.gamesPlayed = 0;
-        this.gamesWon = 0;
-        this.currentSnipe = null;
-    }
-
-    public getWinRate() {
-        // Calculation with some javascript rounding magic
-        return this.gamesPlayed > 0 ? Math.round(this.gamesWon / this.gamesPlayed * 10000) / 100 : 0;
-    }
-
-    public generateNewSnipe() {
-        this.currentSnipe = Math.floor(Math.random() * 99) + 1;
-    }
-}
-
-const getSnipers = (brain: Brain): Record<string, Sniper> => {
-    // If the brain has no knowledge of sniperino, create it
-    if (brain.get('sniperino') === null) {
-        brain.set('sniperino', {});
-    }
-
-    return brain.get('sniperino');
-}
-
-// Get a sniper with a given name. If one doesn't exist, create it.
-const getOrCreateSniper = (brain: Brain, user: User, create: boolean) => {
-    const snipers = getSnipers(brain);
-
-    // Check for active sniper, if not, create one
-    if (snipers[user.id] === undefined && create) {
-        snipers[user.id] = new Sniper(user);
-    }
-
-    return snipers[user.id];
+const stringFns: Record<GameEvent, (c: GameContext) => string> = {
+    [GameEvent.New]: ({ name, snipe }) =>
+        `ヽ༼ຈل͜ຈ༽_•︻̷̿┻̿═━一 ༼ つ ಥ_ಥ ༽つ ${name}, roll higher than a ${snipe} or the donger gets it!`,
+    [GameEvent.Dupe]: ({ name }) =>
+        `(ノಠ益ಠ)ノ彡┻━┻ YOU ARE ALREADY PLAYING SNIPERINO, ${name}. I oughtta sniperino YOU! ༼ຈل͜ຈ༽_•︻̷̿┻̿═━一`,
+    [GameEvent.Win]: ({ name, roll }) =>
+        `(◠‿◠✿) ${name}, you roll a ${roll} and the donger lives! The donger thanks you (◠‿◠✿)`,
+    [GameEvent.Draw]: ({ name, roll }) =>
+        `ヽ༼ຈل͜ຈ༽/ ${name}, you roll a ${roll} and tie! The donger is merely wounded. He will recover! ヽ༼ຈل͜ຈ༽/`,
+    [GameEvent.Lose]: ({ name }) =>
+        `༼ つ x_x ༽つThe donger is dead. ${name}, you did this! You MONSTER! ༼ つ x_x ༽ つ`,
 };
 
 export = (robot: Robot) => {
-
-    // Respond to a stats message
     robot.respond(/sniperino stats( me)?/i, res => {
-        const snipers = getSnipers(robot.brain);
-        const stats = Object.keys(snipers)
-            .sort((a, b) => snipers[b].getWinRate() - snipers[a].getWinRate())
-            .map(n => `${snipers[n].name}: ${snipers[n].getWinRate()}%`);
+        const { stats } = loadState(robot.brain);
+        const message = Object.values(stats)
+            .filter(<T>(x?: T): x is T => x !== undefined)
+            .map(stat => ({ ...stat, winRate: calculateWinRate(stat) }))
+            .sort((a, b) => b.winRate - a.winRate)
+            .map(({ userId, winRate }) => `${robot.brain.userForId(userId).name}: ${winRate}%`)
+            .join('\n');
 
-        res.send(stats.join('\n'));
+        res.send(message);
     });
 
-    // Respond to a sniperino message
     robot.respond(/sniperino( me)?$/i, res => {
-        // Define instance of our Sniper object from getSniper method
-        const sniper = getOrCreateSniper(robot.brain, res.message.user, true);
-
-        // Deny the play if someone is already actively sniping. Otheriwise, display their roll.
-        if (sniper.currentSnipe === null) {
-            // Player isn't playing, start a game for them
-            sniper.generateNewSnipe();
-
-            // Send their game information
-            res.send(`ヽ༼ຈل͜ຈ༽_•︻̷̿┻̿═━一 ༼ つ ಥ_ಥ ༽つ${sniper.name}, roll higher than a ${sniper.currentSnipe} or the donger gets it!`);
+        const state = loadState(robot.brain);
+        const { name, id } = res.message.user;
+        if (state.games[id] === undefined) {
+            const snipe = Math.floor(Math.random() * 99) + 1;
+            state.games[id] = snipe;
+            res.send(stringFns[GameEvent.New]({ name, snipe }));
         } else {
-            // Player is already playing, yell at them because they are an idiot
-            res.send(`(ノಠ益ಠ)ノ彡┻━┻ YOU ARE ALREADY PLAYING SNIPERINO, ${sniper.name}. I oughtta sniperino YOU! ༼ຈل͜ຈ༽_•︻̷̿┻̿═━一`);
+            res.send(stringFns[GameEvent.Dupe]({ name }));
         }
     });
 
     robot.on('roll', (res: Response, roll: number, max: number) => {
-        // Get our currently rolling sniper
-        const sniper = getOrCreateSniper(robot.brain, res.message.user, false);
-
-        // If this is a valid sniperino roll and we have an active sniperino, resolve the game
-        if (max === 100 && sniper.currentSnipe) {
-            if (roll > sniper.currentSnipe) {
-                res.send(`(◠‿◠✿) ${sniper.name}, you roll a ${roll} and the donger lives! The donger thanks you (◠‿◠✿)`);
-                sniper.gamesWon += 1;
-            } else if (roll === sniper.currentSnipe) {
-                res.send(`ヽ༼ຈل͜ຈ༽/ ${sniper.name}, you roll a ${roll} and tie! The donger is merely wounded. He will recover! ヽ༼ຈل͜ຈ༽/`);
-            } else {
-                res.send(`༼ つ x_x ༽つThe donger is dead. ${sniper.name}, you did this! You MONSTER! ༼ つ x_x ༽ つ`);
-            }
-
-            sniper.gamesPlayed += 1;
-            sniper.currentSnipe = null;
+        // If someone rolled out of something other than 100, they are
+        // either not playing sniperino or trying to cheat.
+        if (max !== 100) {
+            return;
         }
+
+        // If there are no active games then this was just an ordinary roll
+        const state = loadState(robot.brain);
+        const { name, id } = res.message.user;
+        const snipe = state.games[id];
+        if (snipe === undefined) {
+            return;
+        }
+
+        const {
+            gamesPlayed = 0,
+            gamesWon = 0
+        } = state.stats[id] || {};
+
+        // Play game
+        const event = playGame(roll, snipe);
+        state.stats[id] = {
+            gamesPlayed: (event !== GameEvent.Draw ? gamesPlayed + 1 : gamesPlayed),
+            gamesWon: (event === GameEvent.Win ? gamesWon + 1 : gamesWon),
+            userId: id,
+        };
+
+        res.send(stringFns[event]({ name, roll }));
+
+        delete state.games[id];
     });
 };
