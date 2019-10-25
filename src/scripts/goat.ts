e56a451020d58750aba1bb4c812f83492606b99d
Steve Shipsey <sshipsey@sidearmsports.com>
2019-10-24 21:42:12 -0400

    goat script

---
+++ src/scripts/goat.ts
@@ -0,0 +1,15 @@
+import { Robot } from 'hubot';
+
+const getGoatLink = (): string => {
+    const random = () => Math.floor(Math.random() * 20);
+
+    if (random() === 1) {
+        return 'https://i.imgur.com/eHd8Oy7.jpg';
+    }
+    return `https://placegoat.com/400/400?cachebust=${random() * random()}`;
+};
+
+export = (robot: Robot) =>
+    robot.respond(/(goat)( me)?/i, async res => {
+        res.send(getGoatLink());
+    });
