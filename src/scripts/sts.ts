ef2316de67c9d669dbe1ce126e8b7e4b762d5552
Steve Shipsey <sashipsey@gmail.com>
2019-11-11 20:25:37 -0500

    fixed strike/defend. removed autocapitalization as it is inconsistent across cards. better checking for non-existant cards (#37)

--- src/scripts/sts.ts
+++ src/scripts/sts.ts
@@ -4,12 +4,7 @@ import { Robot } from 'hubot';
 
 export = (robot: Robot) =>
     robot.respond(/sts( me)?( (.+))/i, async res => {
-        const card = res.match[2]
-            .trim()
-            .split(' ')
-            .map(word => word.toLowerCase())
-            .map(word => word.replace(/^\w/, c => c.toUpperCase()))
-            .join('_');
+        const card = res.match[2].trim().replace(' ', '_');
 
         return otherCards[card]
             ? res.send(otherCards[card])
@@ -17,9 +12,9 @@ export = (robot: Robot) =>
     });
 
 const otherCards: { [key: string]: string } = {
-    defend:
+    Defend:
         'https://vignette.wikia.nocookie.net/slay-the-spire/images/7/7d/Defend_R.png/revision/latest/scale-to-width-down/620?cb=20181016205732',
-    strike:
+    Strike:
         'https://vignette.wikia.nocookie.net/slay-the-spire/images/0/06/Strike_R.png/revision/latest/scale-to-width-down/620?cb=20181016211045'
 };
 
@@ -27,5 +22,10 @@ const fetchCard = async (card: string) => {
     const BASE_URL = 'https://slay-the-spire.fandom.com/wiki';
     const result = await axios.get(`${BASE_URL}/${card}`);
     const $ = cheerio.load(result.data);
-    return $('.pi-image-thumbnail')[0].attribs['src'] || 'Card not found';
+    return (
+        ($('.pi-image-thumbnail') &&
+            $('.pi-image-thumbnail')[0] &&
+            $('.pi-image-thumbnail')[0].attribs['src']) ||
+        'Card not found'
+    );
 };
