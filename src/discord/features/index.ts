4bb68bf8e542a0e1191c1f780ac67fdd91160124
Matthew Perry <muffinman616@gmail.com>
2024-08-24 02:21:47 -0400

    Start DB3

--- src/discord/features/index.ts
+++
@@ -1,5 +0,0 @@
-import DictateFeature from './dictateFeature';
-import GamesNotificationFeature from './gamesNotificationFeature';
-import MusicPlayerFeature from './musicPlayerFeature';
-
-export default [GamesNotificationFeature, MusicPlayerFeature, DictateFeature];
